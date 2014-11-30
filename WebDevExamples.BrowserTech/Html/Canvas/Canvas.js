59c722290e7bf7142547133d36954fc7c059e847
scch1002 <chrisschwan8009@hotmail.com>
2014-11-30 04:02:54 -0600

    Created Array Api example and created Canvas Example

---
+++ WebDevExamples.BrowserTech/Html/Canvas/Canvas.js
@@ -0,0 +1,48 @@
+﻿var webDevExamples = webDevExamples || {};
+webDevExamples.html = webDevExamples.html || {};
+webDevExamples.html.canvas = webDevExamples.html.canvas || {};
+
+(function () {
+    var canvas = this;
+
+    canvas.drawball = function (canvasContext, x, y, radius, HexColor) {
+        canvasContext.fillStyle = HexColor;
+        canvasContext.beginPath();
+        canvasContext.arc(x, y, radius, 0, Math.PI * 2, true);
+        canvasContext.fill();
+    };
+
+    canvas.paintDrops = function (canvasContext, canvasWidth, canvasHeight, maxRadius) {
+        setInterval(function () {
+            var y = Math.floor(Math.random() * (canvasHeight - 0)) + 0;
+            var x = Math.floor(Math.random() * (canvasWidth - 0)) + 0;
+            var radius = Math.floor(Math.random() * (maxRadius - 1)) + 1;
+            var hexColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+
+            canvas.drawball(canvasContext, x, y, radius, hexColor);
+        }, 1000);
+    };
+
+    canvas.resizeCanvas = function () {
+        var canvasExample = $('#canvasExample')[0];
+        var canvasContainer = $('#canvasContainer')[0];
+
+        canvasExample.width = canvasContainer.clientWidth;
+        canvasExample.height = canvasContainer.clientHeight;
+    };
+
+    canvas.init = function () {
+        var canvasExample = document.getElementById('canvasExample');
+        if (canvasExample.getContext) {
+            var canvasContext = canvasExample.getContext('2d');
+            canvas.paintDrops(canvasContext, 500, 500, 50);
+            $(window).on('resize', canvas.resizeCanvas);
+            canvas.resizeCanvas();
+        }
+    };
+
+}).call(webDevExamples.html.canvas);
+
+$(document).ready(function () {
+    webDevExamples.html.canvas.init();
+});
---
+++ WebDevExamples.BrowserTech/Javascript/ArrayApi/ArrayApi.js
@@ -0,0 +1,91 @@
+﻿var webDevExamples = webDevExamples || {};
+webDevExamples.javascript = webDevExamples.javascript || {};
+webDevExamples.javascript.arrayapi = webDevExamples.javascript.arrayapi || {};
+
+(function () {
+    var arrayapi = this;
+
+    var exampleArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+    
+    arrayapi.popItem = function (event) {
+        var value = exampleArray.pop();
+        arrayapi.showReturnValueModal(value);
+        arrayapi.clearArrayDisplay();
+        arrayapi.populateArrayDisplay();
+    };
+
+    arrayapi.pushItem = function (event) {
+        value = $('#pushValue').val();
+        var value = exampleArray.push(value);
+        arrayapi.clearArrayDisplay();
+        arrayapi.populateArrayDisplay();
+    };
+
+    arrayapi.reverseItems = function (event) {
+        exampleArray.reverse();
+        arrayapi.clearArrayDisplay();
+        arrayapi.populateArrayDisplay();
+    };
+
+    arrayapi.shiftItem = function (event) {
+        var value = exampleArray.shift();
+        arrayapi.showReturnValueModal(value);
+        arrayapi.clearArrayDisplay();
+        arrayapi.populateArrayDisplay();
+    };
+
+    arrayapi.unshiftItem = function (event) {
+        value = $('#unShiftValue').val();
+        var value = exampleArray.unshift(value);
+        arrayapi.clearArrayDisplay();
+        arrayapi.populateArrayDisplay();
+    };
+
+    arrayapi.sortItems = function (event) {
+        exampleArray.sort();
+        arrayapi.clearArrayDisplay();
+        arrayapi.populateArrayDisplay();
+    };
+
+    arrayapi.showReturnValueModal = function (value) {
+        $('#modalBody').text(value);
+        $('#returnedValueModal').modal('show');
+    };
+
+    arrayapi.populateArrayDisplay = function () {
+        var arrayExampleDisplay = $('#arrayExampleDisplay')[0];
+        exampleArray.forEach(function (value, index, array) {
+            arrayExampleDisplay.appendChild(arrayapi.constructArrayItemDisplay(index, value));
+        });
+    };
+
+    arrayapi.clearArrayDisplay = function () {
+        arrayExampleDisplay.innerHTML = '';
+    };
+
+    arrayapi.constructArrayItemDisplay = function (index, item) {
+        var row = document.createElement('tr');
+        var itemIndexColumn = document.createElement('td');
+        var itemColumn = document.createElement('td');
+        itemIndexColumn.innerHTML = index;
+        itemColumn.innerHTML = item;
+        row.appendChild(itemIndexColumn);
+        row.appendChild(itemColumn);
+        return row;
+    };
+
+    arrayapi.init = function () {
+        arrayapi.populateArrayDisplay();
+        $('#popItem').click(arrayapi.popItem);
+        $('#pushItem').click(arrayapi.pushItem);
+        $('#reverseItems').click(arrayapi.reverseItems);
+        $('#shiftItem').click(arrayapi.shiftItem);
+        $('#unshiftItem').click(arrayapi.unshiftItem);
+        $('#sortItems').click(arrayapi.sortItems);
+    };
+
+}).call(webDevExamples.javascript.arrayapi);
+
+$(document).ready(function () {
+    webDevExamples.javascript.arrayapi.init();
+});
