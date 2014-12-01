524f313fc1c8e4863b7961a983783bd5052a02d0
scch1002 <chrisschwan8009@hotmail.com>
2014-11-30 22:20:58 -0600

    Create Html Element Examples page and started on from example page

---
+++ WebDevExamples.BrowserTech/Html/HtmlElements/HtmlElements.js
@@ -0,0 +1,16 @@
+﻿var webDevExamples = webDevExamples || {};
+webDevExamples.html = webDevExamples.html || {};
+webDevExamples.html.htmlElementExamples = webDevExamples.html.htmlElementExamples || {};
+
+(function () {
+    var htmlElementExamples = this;
+
+    htmlElementExamples.init = function () {
+
+    };
+
+}).call(webDevExamples.html.htmlElementExamples);
+
+$(document).ready(function () {
+    webDevExamples.html.htmlElementExamples.init();
+});
