ca3021279fe30b689b9c9ab69be56d20fc36a322
scch1002 <chrisschwan8009@hotmail.com>
2014-12-06 20:23:50 -0600

    Added web api controller for web service

---
+++ WebDevExamples.ServerWebTech/Controllers/api/ExamplesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebDevExamples.ServerWebTech.Controllers.api
+{
+    public class ExamplesController : ApiController
+    {
+        public List<string> Get()
+        {
+            return "This is an example of an web api controller".Split(' ').ToList();
+        }
+
+        public List<string> Post(string text)
+        {
+            return text.Split(' ').ToList();
+        }
+
+        public List<string> Put(string text)
+        {
+            return text.Split(' ').Select(s => "{" + s + "}").ToList();
+        }
+    }
+}
