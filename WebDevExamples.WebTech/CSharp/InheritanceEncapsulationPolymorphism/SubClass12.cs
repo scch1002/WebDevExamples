cd6075b7b2b40799eb8ad11b56c1c271fca4302c
scch1002 <chrisschwan8009@hotmail.com>
2014-12-13 22:56:21 -0600

    Updated SubClass12 so that it works correctly

--- WebDevExamples.WebTech/CSharp/InheritanceEncapsulationPolymorphism/SubClass12.cs
+++ WebDevExamples.WebTech/CSharp/InheritanceEncapsulationPolymorphism/SubClass12.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace WebDevExamples.WebTech.CSharp.InheritanceEncapsulationPolymorphism
 {
-    public class SubClass12 : SubClass1
+    public class SubClass12 : SubClass1, IOutputMethod1And2
     {
         public new int Method()
         {
