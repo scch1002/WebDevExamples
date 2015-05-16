819acefd545f2eae203951bb4eb3a5cd61eca012
scch1002 <chrisschwan8009@hotmail.com>
2015-05-15 21:00:30 -0500

    Started creating delegate example

---
+++ WebDevExamples.WebTech/CSharp/Delegates/Delegates.js
@@ -0,0 +1,16 @@
+﻿var webDevExamples = webDevExamples || {};
+webDevExamples.CSharp = webDevExamples.CSharp || {};
+webDevExamples.CSharp.Delegates = webDevExamples.CSharp.Delegates || {};
+
+(function () {
+    var delegates = this;
+
+    delegates.init = function () {
+
+    };
+
+}).call(webDevExamples.html.Delegates);
+
+$(document).ready(function () {
+    webDevExamples.html.Delegates.init();
+});
