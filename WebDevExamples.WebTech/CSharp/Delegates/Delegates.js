7f7c7086064e641db4daecdb9ba86b3fbc35ee06
scch1002 <chrisschwan8009@hotmail.com>
2015-05-19 21:07:48 -0500

    Created basic delegate example

--- WebDevExamples.WebTech/CSharp/Delegates/Delegates.js
+++ WebDevExamples.WebTech/CSharp/Delegates/Delegates.js
@@ -5,12 +5,30 @@ webDevExamples.CSharp.Delegates = webDevExamples.CSharp.Delegates || {};
 (function () {
     var delegates = this;
 
-    delegates.init = function () {
-
+    delegates.constructListItem = function (text) {
+        var div = document.createElement('div');
+        div.innerHTML = text;
+        div.classList.add('list-group-item');
+        return div;
     };
 
-}).call(webDevExamples.html.Delegates);
+    delegates.hello = function (hello) {
+        $('#basicDelegate').append(delegates.constructListItem(hello));
+    };
+
+    delegates.init = function () {
+        var broadcaster = $.connection.delegatesHub
+        broadcaster.client.hello = delegates.hello;
+        $.connection.hub.start().done(function () {
+            $('#BasicDelegateButton').click(function () {
+                broadcaster.server.hello();
+            });
+            broadcaster.server.hello();
+        });
+    };
+
+}).call(webDevExamples.CSharp.Delegates);
 
 $(document).ready(function () {
-    webDevExamples.html.Delegates.init();
+    webDevExamples.CSharp.Delegates.init();
 });
