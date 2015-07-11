ae179688a7e2c64923c041a29eef603973df25a6
scch1002 <chrisschwan8009@hotmail.com>
2015-07-11 18:04:56 -0500

    Started Threading And Synchronization Examples

---
+++ WebDevExamples.WebTech/CSharp/ThreadingAndSynchronization/ThreadingAndSynchronization.js
@@ -0,0 +1,33 @@
+﻿var webDevExamples = webDevExamples || {};
+webDevExamples.CSharp = webDevExamples.CSharp || {};
+webDevExamples.CSharp.ThreadingAndSynchronization = webDevExamples.CSharp.ThreadingAndSynchronization || {};
+
+(function () {
+    var threadingAndSynchronization = this;
+
+    threadingAndSynchronization.constructListItem = function (text) {
+        var div = document.createElement('div');
+        div.innerHTML = text;
+        div.classList.add('list-group-item');
+        return div;
+    };
+
+    threadingAndSynchronization.simpleThreadingExample = function (response) {
+        $('#simpleThreadingExample').append(threadingAndSynchronization.constructListItem(response));
+    };
+
+    threadingAndSynchronization.init = function () {
+        var broadcaster = $.connection.threadingAndSynchronizationHub
+        broadcaster.client.simpleThreadingExample = threadingAndSynchronization.simpleThreadingExample;
+        $.connection.hub.start().done(function () {
+            $('#simpleThreadingExampleButton').click(function () {
+                broadcaster.server.simpleThreadingExample();
+            });
+        });
+    };
+
+}).call(webDevExamples.CSharp.ThreadingAndSynchronization);
+
+$(document).ready(function () {
+    webDevExamples.CSharp.ThreadingAndSynchronization.init();
+});
---
+++ WebDevExamples.WebTech/CSharp/ThreadingAndSynchronization/ThreadingAndSynchronizationHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace WebDevExamples.WebTech.CSharp.ThreadingAndSynchronization
+{
+    public class ThreadingAndSynchronizationHub : Hub
+    {
+        public void SimpleThreadingExample()
+        {
+            var simpleThread = new Thread(() =>
+            {
+                Clients.Caller.SimpleThreadingExample("Simple threading example has started.");
+                for (var count = 0; count <= 5; count++)
+                {
+                    Thread.Sleep(2000);
+                    Clients.Caller.SimpleThreadingExample("Simple Threading Example: " + count);
+                }
+                Thread.Sleep(2000);
+                Clients.Caller.SimpleThreadingExample("Simple threading example has ended.");
+            });
+            simpleThread.Start();
+        }
+    }
+}
