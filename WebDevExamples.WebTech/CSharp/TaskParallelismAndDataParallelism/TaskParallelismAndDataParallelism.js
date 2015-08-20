646c775bf4228d36d812397c32849de149788cd0
scch1002 <chrisschwan8009@hotmail.com>
2015-08-20 00:58:17 -0500

    Started Task Parallelism and Data Parallelism examples

---
+++ WebDevExamples.WebTech/CSharp/TaskParallelismAndDataParallelism/TaskParallelismAndDataParallelism.js
@@ -0,0 +1,33 @@
+﻿var webDevExamples = webDevExamples || {};
+webDevExamples.CSharp = webDevExamples.CSharp || {};
+webDevExamples.CSharp.TaskParallelismAndDataParallelism = webDevExamples.CSharp.TaskParallelismAndDataParallelism || {};
+
+(function () {
+    var taskParallelismAndDataParallelism = this;
+
+    taskParallelismAndDataParallelism.constructListItem = function (text) {
+        var div = document.createElement('div');
+        div.innerHTML = text;
+        div.classList.add('list-group-item');
+        return div;
+    };
+
+    taskParallelismAndDataParallelism.simpleTaskExample = function (response) {
+        $('#simpleTaskExample').append(taskParallelismAndDataParallelism.constructListItem(response));
+    };
+
+    taskParallelismAndDataParallelism.init = function () {
+        var broadcaster = $.connection.taskParallelismAndDataParallelismHub;
+        broadcaster.client.simpleTaskExample = taskParallelismAndDataParallelism.simpleTaskExample;
+        $.connection.hub.start().done(function () {
+            $('#simpleTaskExampleButton').click(function () {
+                broadcaster.server.simpleTaskExample();
+            });
+        });
+    };
+
+}).call(webDevExamples.CSharp.TaskParallelismAndDataParallelism);
+
+$(document).ready(function () {
+    webDevExamples.CSharp.TaskParallelismAndDataParallelism.init();
+});
---
+++ WebDevExamples.WebTech/CSharp/TaskParallelismAndDataParallelism/TaskParallelismAndDataParallelismHub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace WebDevExamples.WebTech.CSharp.TaskParallelismAndDataParallelism
+{
+    public class TaskParallelismAndDataParallelismHub : Hub
+    {
+        public void SimpleTaskExample()
+        {
+            var simpleTask = new Task<string>(() => 
+            {
+                Thread.Sleep(2000);
+                return "This is a simple task example";
+            });
+            simpleTask.Start();
+            Clients.Caller.SimpleTaskExample(simpleTask.Result);
+        }
+    }
+}
